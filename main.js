e6976995c3a1c1f274ebcb821b168f42ceb34403
karolinagabrys0211-ship-it <karolina.gabrys0211@gmail.com>
2026-02-26 20:06:18 +0100

    Navbar fix
    I fixed the navigationbar and some margins

---
+++ main.js
@@ -0,0 +1,7 @@
+// const button = document.getElementById("read-more");
+// const bodyElement = document.querySelector("body");
+
+// button.addEventListener("click", function (event) {
+//    bodyElement.style.backgroundColor = "#ff0000";
+// });
+
