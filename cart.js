33c67da2f3c7ed7e6413e0db999cce2a4041e34c
karolinagabrys0211-ship-it <karolina.gabrys0211@gmail.com>
2026-03-12 00:10:08 +0100

    Real final commit
    A few files was missing

---
+++ cart.js
@@ -0,0 +1,53 @@
+const cartLink = document.getElementById("cart-link");
+const cartPopup = document.getElementById("cart-popup");
+const closeCart = document.getElementById("close-cart");
+const clearCart = document.getElementById("clear-cart");
+
+//The next 18 lines of code was adapted from https://www.youtube.com/watch?v=IY5UN82FZ2Q accessed 2026-03-02
+
+function displayCart() {
+
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const container = document.getElementById("cart-container");
+    container.innerHTML = "";
+
+    if (cart.length === 0) {
+        container.innerHTML = "<p>Cart is empty</p>";
+        return;
+    }
+
+    cart.forEach(function(item) {
+        container.innerHTML +=
+            "<p class='cart-item'>" +
+            item.name + " - " + item.price + "kr" +
+            "</p>";
+    });
+}
+
+displayCart();
+
+cartLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    cartPopup.classList.remove("hidden");
+    displayCart();
+});
+
+
+// Close cart popup
+closeCart.addEventListener("click", () => {
+    cartPopup.classList.add("hidden");
+});
+
+// Click outside cart content
+cartPopup.addEventListener("click", (e) => {
+    if (e.target === cartPopup) {
+        cartPopup.classList.add("hidden");
+    }
+});
+
+
+// Clear cart button
+clearCart.addEventListener("click", () => {
+    localStorage.removeItem("cart");
+    displayCart();
+});
---
+++ product.js
@@ -0,0 +1,76 @@
+const buttonOne = document.getElementById("book-one");
+const buttonTwo = document.getElementById("book-two");
+const buttonThree = document.getElementById("book-three");
+
+const cartLink = document.getElementById("cart-link");
+const cartPopup = document.getElementById("cart-popup");
+const closeCart = document.getElementById("close-cart");
+const clearCart = document.getElementById("clear-cart");
+
+//The next 12 lines of code was adapted from https://www.youtube.com/watch?v=PoTGs38DR9E accessed 2026-02-26
+
+function addToCart(id, name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({
+        id: id,
+        name: name,
+        price: price
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    displayCart();
+}
+
+buttonOne.addEventListener("click", function () {
+    addToCart(1, "Book-one", 50);
+});
+
+//The next 18 lines of code was adapted from https://www.youtube.com/watch?v=IY5UN82FZ2Q accessed 2026-03-02
+
+function displayCart() {
+
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const container = document.getElementById("cart-container");
+    container.innerHTML = "";
+
+    if (cart.length === 0) {
+        container.innerHTML = "<p>Cart is empty</p>";
+        return;
+    }
+
+    cart.forEach(function(item) {
+        container.innerHTML +=
+            "<p class='cart-item'>" +
+            item.name + " - " + item.price + "kr" +
+            "</p>";
+    });
+}
+
+displayCart();
+
+cartLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    cartPopup.classList.remove("hidden");
+    displayCart();
+});
+
+
+// Close cart popup
+closeCart.addEventListener("click", () => {
+    cartPopup.classList.add("hidden");
+});
+
+// Click outside cart content
+cartPopup.addEventListener("click", (e) => {
+    if (e.target === cartPopup) {
+        cartPopup.classList.add("hidden");
+    }
+});
+
+
+// Clear cart button
+clearCart.addEventListener("click", () => {
+    localStorage.removeItem("cart");
+    displayCart();
+});
